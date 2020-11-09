265727b19376a6fe2bf2ecf83014dc870289e43c
Hirem Pervaz <hirem.p@gmail.com>
2020-11-09 12:20:58 +0400

    Corrected the owner_id attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_08_131645) do
+ActiveRecord::Schema.define(version: 2020_11_09_080445) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,8 +36,8 @@ ActiveRecord::Schema.define(version: 2020_11_08_131645) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "owner_id_id", null: false
-    t.index ["owner_id_id"], name: "index_spaces_on_owner_id_id"
+    t.bigint "owner_id", null: false
+    t.index ["owner_id"], name: "index_spaces_on_owner_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -52,5 +52,5 @@ ActiveRecord::Schema.define(version: 2020_11_08_131645) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "spaces", "users", column: "owner_id_id"
+  add_foreign_key "spaces", "users", column: "owner_id"
 end
