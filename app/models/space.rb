8e7651ee15a2ecc32436715b7dc5bc4e62014a76
Tariq Ismail <tariqismail@gmail.com>
2020-11-09 15:15:07 +0400

    added inclusion validator for area and select form compononet for area in the spaces form

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,10 +1,10 @@
 class Space < ApplicationRecord
   has_many :bookings, dependent: :restrict_with_error
 
+  ZONES = ['Jumeirah 1', 'DIFC', 'Dubai Marina', 'Dubai Knowledge Park', 'Downtown Dubai', 'Business Bay', 'The Palm Jumeirah']
   validates :name, presence: true
   validates :description, presence: true
   validates :capacity, presence: true
   validates :price_per_day, presence: true
-  validates :area, inclusion: { in: %w['Jumeirah 1' DIFC 'Dubai Marina' 'Dubai Knowledge Park' 'Downtown Dubai' 'Business Bay' 'The Palm Jumeirah'] }
-
+  validates :area, inclusion: { in: Space::ZONES }
 end
