558b43ea47fcc52f05a75e53d46ba65ee2018f4b
Tariq Ismail <tariqismail@gmail.com>
2020-11-09 15:54:29 +0400

    completed create action, redirect not working as show action not completed

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -4,6 +4,23 @@ class SpacesController < ApplicationController
     @space = Space.new
   end
 
+  def create
+    @space = Space.new(space_params)
+    @space.owner_id = current_user.id
+
+    if @space.save
+      redirect_to @space
+    else
+      render :new
+    end
+  end
+
   def show
+  end
+
+  private
+
+  def space_params
+    params.require(:space).permit(:name, :capacity, :area, :price_per_day, :description, :photo)
   end
 end
