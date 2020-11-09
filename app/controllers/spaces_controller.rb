af3d39e9df51bcfef87fa43b27ab67c3fda8c78e
Hirem Pervaz <hirem.p@gmail.com>
2020-11-09 18:02:45 +0400

    Created space show page

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,4 +1,10 @@
 class SpacesController < ApplicationController
   def show
+    @space = Space.find(params[:id])
+  end
+
+  private
+  def space_params
+    params.require(:space).permit(:name, :price, :start_date, :end_date, :capacity)
   end
 end
