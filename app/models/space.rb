1c1982a632699fb508b7320bebfac5b8e3453c0a
Tariq Ismail <tariqismail@gmail.com>
2020-11-09 15:04:34 +0400

    added validations to Space model

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,4 +1,10 @@
 class Space < ApplicationRecord
   has_many :bookings, dependent: :restrict_with_error
 
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :capacity, presence: true
+  validates :price_per_day, presence: true
+  validates :area, inclusion: { in: %w['Jumeirah 1' DIFC 'Dubai Marina' 'Dubai Knowledge Park' 'Downtown Dubai' 'Business Bay' 'The Palm Jumeirah'] }
+
 end
