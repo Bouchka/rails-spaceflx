4090ea9ad352fe04e541e55378884e625981520b
Tariq Ismail <tariqismail@gmail.com>
2020-11-09 15:34:41 +0400

    added photo to space model

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,4 +1,5 @@
 class Space < ApplicationRecord
+  has_one_attached :photo
   has_many :bookings, dependent: :restrict_with_error
 
   ZONES = ['Jumeirah 1', 'DIFC', 'Dubai Marina', 'Dubai Knowledge Park', 'Downtown Dubai', 'Business Bay', 'The Palm Jumeirah']
