5fba2dec801563bd50af6c0019be645a3390ebbb
Tariq Ismail <tariqismail@gmail.com>
2020-11-10 15:41:20 +0400

    changed number of users to created in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,7 +15,7 @@ User.destroy_all
 puts "DB sweeped clean"
 
 puts "Creating Users..."
-5.times do
+10.times do
   first_name = Faker::Name.first_name
   last_name = Faker::Name.last_name
   user = User.new(
