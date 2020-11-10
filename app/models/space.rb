bb5ece7be40abbc8288ed52fd52cddcbf3c33583
Tariq Ismail <tariqismail@gmail.com>
2020-11-10 14:07:26 +0400

    added address field and gecode feature to space model

--- app/models/space.rb
+++ app/models/space.rb
@@ -1,7 +1,7 @@
 class Space < ApplicationRecord
   has_one_attached :photo
   geocoded_by :address
-  afer_validation :geocode, if: :will_save_change_to_address?
+  after_validation :geocode, if: :will_save_change_to_address?
   has_many :bookings, dependent: :restrict_with_error
 
   ZONES = ['Jumeirah 1', 'DIFC', 'Dubai Marina', 'Dubai Knowledge Park', 'Downtown Dubai', 'Business Bay', 'The Palm Jumeirah']
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_10_075544) do
+ActiveRecord::Schema.define(version: 2020_11_10_095957) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2020_11_10_075544) do
     t.bigint "owner_id", null: false
     t.float "latitude"
     t.float "longitude"
+    t.string "address"
     t.index ["owner_id"], name: "index_spaces_on_owner_id"
   end
 
@@ -71,6 +72,8 @@ ActiveRecord::Schema.define(version: 2020_11_10_075544) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
