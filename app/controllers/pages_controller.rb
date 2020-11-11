0c98c1b8812754638e1d76327e9f38da16136d9d
Hirem Pervaz <hirem.p@gmail.com>
2020-11-11 19:14:12 +0400

    Spaces sample done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @spaces = Space.all
+    @spaces = Space.all.order(created_at: :desc).limit(6)
   end
 end
